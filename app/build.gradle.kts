9e2e5c6d05ac493dd14435299fd2993eca84cf9b
Andres <124212022+AndresIsCoding@users.noreply.github.com>
2025-05-08 13:08:13 -0500

    Gradle y .gitignore

---
+++ app/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.equiposeis"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.equiposeis"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    sourceSets {
+        getByName("main") {
+            java.srcDirs("src/main/kotlin")
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("android.arch.navigation:navigation-fragment-ktx:1.0.0")
+    implementation("android.arch.navigation:navigation-ui-ktx:1.0.0")
+    implementation("androidx.biometric:biometric:1.2.0-alpha05")
+    implementation("com.airbnb.android:lottie:6.1.0")
+
+
+
+}
