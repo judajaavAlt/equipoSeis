15f5e4236460093c45bee8a45eba288535836723
natanMedina <166678478+natanMedina@users.noreply.github.com>
2025-05-25 19:12:20 -0500

    Room Database
    Los items de la lista son los datos de Room

--- app/src/main/kotlin/database/DataBase.kt
+++ app/src/main/kotlin/database/DataBase.kt
@@ -1,8 +1,27 @@
 package com.equiposeis.database
+
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Pet::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun petDao(): PetDao
+
+    companion object {
+        @Volatile private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "pet_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
